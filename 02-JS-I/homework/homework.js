b495c1d3346aa449de1a5fbf18ca0726662067d8
Gaston Tolesano <gastoontole@gmail.com>
2022-05-23 22:21:20 +0200

    Homework 1 edit 35

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -250,36 +250,42 @@ function deEuroAdolar(euro){
 }
 
 
-function esVocal(letra){
+//function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+ // if (letra.length !==1)
+ // {return  'Dato incorrecto'}
+  
+ // else if (letra=="a")
+ // {return  'Es vocal'}
+  
+ // else if  (letra=='e')
+  //{return  'Es vocal'}
+
+  //else if (letra=='i')
+ // {return  'Es vocal'}
+
+  //else if (letra=='o')
+ // {return  'Es vocal'}
+
+ // else if (letra=='u')
+//  {return  'Es vocal'}
+
+ // else {return "Dato incorrecto"}
+
+//}
+function esVocal(letra){
   if (letra.length !==1)
   {return  'Dato incorrecto'}
-  
-  else if (letra=="a")
-  {return  'Es vocal'}
-  
-  else if  (letra=='e')
-  {return  'Es vocal'}
-
-  else if (letra=='i')
-  {return  'Es vocal'}
-
-  else if (letra=='o')
-  {return  'Es vocal'}
-
-  else if (letra=='u')
-  {return  'Es vocal'}
-
-  else {return "Dato incorrecto"}
-
+   
+   else if (letra=="a" || letra=='e' || letra=='i'|| letra=='o' || letra=='u')
+   {return  'Es vocal'}
+   
+    else {return "Dato incorrecto"}
 }
-
-
-
 // No modificar nada debajo de esta línea
 // --------------------------------
 
